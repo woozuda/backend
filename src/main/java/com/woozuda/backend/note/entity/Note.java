9841417a7d41035ce33220dfb25b4c1c7b00684b
황중섭 <jshwang9709@gmail.com>
2024-11-29 23:15:46 +0900

    feat: Note 엔티티 추가
    DB 스키마 상 슈퍼 타입 테이블인 note에 해당하는 엔티티를 추가했다.

---
+++ src/main/java/com/woozuda/backend/note/entity/Note.java
@@ -0,0 +1,54 @@
+package com.woozuda.backend.note.entity;
+
+import com.woozuda.backend.diary.entity.Diary;
+import com.woozuda.backend.global.entity.BaseTimeEntity;
+import com.woozuda.backend.note.entity.type.Visibility;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "dtype")
+@Table(name = "note")
+public class Note extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "note_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diary_id", updatable = false, nullable = false)
+    private Diary diary;
+
+    @Column(nullable = false)
+    private Long title;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 100, nullable = false)
+    private Visibility visibility;
+
+    @OneToMany(mappedBy = "note", cascade = CascadeType.ALL)
+    private List<NoteContent> contents = new ArrayList<>();
+
+}
