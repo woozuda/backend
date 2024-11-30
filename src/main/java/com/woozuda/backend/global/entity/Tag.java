9686e7ada08fde73b588949a02a9a35106ad1f01
황중섭 <jshwang9709@gmail.com>
2024-11-30 12:04:27 +0900

    feat: 태그의 name 필드 유니크 제약 조건을 @Column에서 @Table로 이동
    유니크 제약조건의 이름을 직접 지정하기 위해 @Table로 이동시켰다.

--- src/main/java/com/woozuda/backend/global/entity/Tag.java
+++ src/main/java/com/woozuda/backend/global/entity/Tag.java
@@ -7,18 +7,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @Table(
         name = "tag",
+        uniqueConstraints = {@UniqueConstraint(name = "unique_name", columnNames = "name")},
         indexes = {@Index(columnList = "name")} //ddl-auto:none 이면 동작 X
 )
-public class Tag extends BaseTimeEntity{
+public class Tag extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "tag_id")
     private Long id;
 
-    @Column(length = 100, unique = true, nullable = false)
+    @Column(length = 100, nullable = false)
     private String name;
 }
