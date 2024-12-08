ff32979ef04163c76210465e60c9428a7f5f4ec8
rodom_macbook <enjoying1018@naver.com>
2024-12-08 20:16:37 +0900

    fix: 오타 수정

--- src/main/java/com/woozuda/backend/swagger/SwaggerConfig.java
+++ src/main/java/com/woozuda/backend/swagger/SwaggerConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 @OpenAPIDefinition(
         servers = {
                 @Server(url = "http://localhost:8080", description = "로컬 서버"),
-                @Server(url = "https://woozuda_release.shop", description = "배포 서버")
+                @Server(url = "https://woozuda-release.shop", description = "배포 서버")
         })
 public class SwaggerConfig {
 
