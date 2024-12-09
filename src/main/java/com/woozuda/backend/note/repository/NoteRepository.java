a7f4c773c25b8930320d0c12a35c4f21ee0d4115
황중섭 <jshwang9709@gmail.com>
2024-12-09 14:29:53 +0900

    feat: AI 분석을 위한 노트 조회

--- src/main/java/com/woozuda/backend/note/repository/NoteRepository.java
+++ src/main/java/com/woozuda/backend/note/repository/NoteRepository.java
@@ -1,5 +1,6 @@
 package com.woozuda.backend.note.repository;
 
+import com.woozuda.backend.forai.repository.CustomNoteRepoForAi;
 import com.woozuda.backend.note.entity.Note;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface NoteRepository extends JpaRepository<Note, Long>, CustomNoteRepository {
+public interface NoteRepository extends JpaRepository<Note, Long>, CustomNoteRepository, CustomNoteRepoForAi {
 
     @Query("select n.date from Note n where n.diary.id in :idList")
     List<LocalDate> findDateByDiaryIds(@Param("idList") List<Long> idList);
