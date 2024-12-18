80a865e738f077f128bb65161aff37d05888531a
rodom_macbook <enjoying1018@naver.com>
2024-12-18 13:07:18 +0900

    feat: id/pw 로그인, 소셜로그인에 user 엔티티에 추가된 칼럼 채우는 로직 적용

--- src/main/java/com/woozuda/backend/account/transdata/KakaoResponse.java
+++ src/main/java/com/woozuda/backend/account/transdata/KakaoResponse.java
@@ -8,11 +8,11 @@ public class KakaoResponse implements OAuth2Response {
     private String email;
     private String name;
 
-    // kakao : {id=3812693211, connected_at=2024-11-29T08:37:05Z, properties={nickname=이동현}, kakao_account={profile_nickname_needs_agreement=false, profile={nickname=이동현, is_default_nickname=false}}}
+    // kakao : {id=3821328965, connected_at=2024-12-05T07:19:11Z, properties={nickname=이동현}, kakao_account={profile_nickname_needs_agreement=false, profile={nickname=이동현, is_default_nickname=false}, has_email=true, email_needs_agreement=false, is_email_valid=true, is_email_verified=true, email=enjoying1018@daum.net}}
     public KakaoResponse(Map<String, Object> attribute){
         this.provider = "kakao";
         this.providerId = attribute.get("id").toString();
-        this.email = attribute.get("email").toString();
+        this.email = ((Map) attribute.get("kakao_account")).get("email").toString();
         this.name = "";
     }
 
