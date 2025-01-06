d28aa39236ae4c0ee5571e21d32a7dc8ccac4afd
황중섭 <jshwang9709@gmail.com>
2025-01-06 19:14:01 +0900

    feat: OpenFeign 설정 및 외부 API와의 통신 클라이언트 생성

---
+++ src/main/java/com/woozuda/backend/question/service/AiQuestionCreationApiClient.java
@@ -0,0 +1,22 @@
+package com.woozuda.backend.question.service;
+
+import com.woozuda.backend.question.service.dto.request.AiQuestionRequestDto;
+import com.woozuda.backend.question.service.dto.response.AiQuestionResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@FeignClient(name = "QuestionCreationApiClient", url = "${ncp.clova-studio.question-creation.url}")
+public interface AiQuestionCreationApiClient {
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    AiQuestionResponseDto makeAiQuestion(
+            @RequestHeader("X-NCP-CLOVASTUDIO-API-KEY") String apiKey,
+            @RequestHeader("X-NCP-APIGW-API-KEY") String apigwKey,
+            @RequestBody AiQuestionRequestDto requestDto
+    );
+
+}
